0650ffca2941f8bc4ef18ca61bbf55709c346d81
Seoyeon Park <101249857+wwwept@users.noreply.github.com>
2023-11-02 13:27:33 +0900

    Update 10807_2.java

--- OneDimensionalArray/10807_2.java
+++ OneDimensionalArray/10807_2.java
@@ -1,7 +1,7 @@
 // ArrayList와 두 가지 loop 선언 후 첫 번째 루프에서 입력을 받고 두 번째 루프에서 v값과 리스트의 값을 비교한 방법
 
 import java.util.*;
-public class Test {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
