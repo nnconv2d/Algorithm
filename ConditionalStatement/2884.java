1dbb7520a305157b0952946c3611f0ecbd086cd5
Seoyeon Park <101249857+wwwept@users.noreply.github.com>
2024-01-08 23:26:30 +0900

    Create 2884.java

---
+++ ConditionalStatement/2884.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        if (m >= 45) {
+            System.out.println(h + " " + (m-45));
+        } else {
+            if (h == 0) {
+                System.out.println("23 " + (m + 15));
+            } else {
+                System.out.println((h - 1) + " " + (m + 15));
+            }
+        }
+    }
+}
