cfbe8570956643b707d7ca2ccf53728f267b0568
Seoyeon Park <101249857+wwwept@users.noreply.github.com>
2024-01-15 22:15:01 +0900

    Create 2501.java

---
+++ Numeric/2501.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        List<Integer> list = new ArrayList<>();
+
+        for (int i=1; i<=n; i++) {
+            if (n%i == 0) {
+                list.add(i);
+            }
+        }
+
+        if (k>list.size()) {
+            System.out.println(0);
+        } else {
+            System.out.println(list.get(k-1));
+        }
+    }
+}
