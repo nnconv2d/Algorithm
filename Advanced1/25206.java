511e9407c4701d66454ed05efcb4c104e37204e4
Seoyeon Park <101249857+wwwept@users.noreply.github.com>
2024-01-29 23:53:22 +0900

    Update 25206.java

--- Advanced1/25206.java
+++ Advanced1/25206.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.math.BigDecimal;
 import java.util.StringTokenizer;
 public class Main {
     public static void main(String args[]) throws IOException {
@@ -6,34 +7,46 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        double sum = 0;
+        BigDecimal sum = new BigDecimal("0.0");
+        BigDecimal len = new BigDecimal("20");
 
         for (int i=0; i<20; i++) {
             st = new StringTokenizer(br.readLine());
-            double score = 0;
+            BigDecimal score = new BigDecimal("0.0");
             st.nextToken();
-            st.nextToken();
+            BigDecimal num = new BigDecimal(st.nextToken());
             String grade = st.nextToken();
-            if (grade == "A+") {
-                score = 4.5;
-            } else if (grade == "A0") {
-                score = 4.0;
-            } else if (grade == "B+") {
-                score = 3.5;
-            } else if (grade == "B0") {
-                score = 3.0;
-            } else if (grade == "C+") {
-                score = 2.5;
-            } else if (grade == "C0") {
-                score = 2.0;
-            } else if (grade == "D+") {
-                score = 1.5;
-            } else if (grade == "D0") {
-                score = 1.0;
-            } else if (grade == "F") {
-                score = 0.0;
+            System.out.println(grade);
+            if (grade.equals("A+")) {
+                score = new BigDecimal("4.5");
+            } else if (grade.equals("A0")) {
+                score = new BigDecimal("4.0");
+            } else if (grade.equals("B+")) {
+                score = new BigDecimal("3.5");
+            } else if (grade.equals("B0")) {
+                score = new BigDecimal("3.0");
+            } else if (grade.equals("C+")) {
+                score = new BigDecimal("2.5");
+            } else if (grade.equals("C0")) {
+                score = new BigDecimal("2.0");
+            } else if (grade.equals("D+")) {
+                score = new BigDecimal("1.5");
+            } else if (grade.equals("D0")) {
+                score = new BigDecimal("1.0");
+            } else if (grade.equals("F")) {
+                score = new BigDecimal("0.0");
+            } else {
+                len = len.subtract(new BigDecimal("1"));
             }
+
+            if (!grade.equals("P")) {
+                sum = sum.add((score.multiply(num)).divide(len));
+            }
+
         }
+
+        System.out.println(len);
+        bw.write(sum+"");
 
         br.close();
         bw.flush();
