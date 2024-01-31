568aed27f659a461d5da554f10cb727139779448
Seoyeon Park <101249857+wwwept@users.noreply.github.com>
2024-01-31 23:36:54 +0900

    Create 2231.java

---
+++ BruteForce/2231.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+        int count = 0;
+
+        for (int j=0; j<n; j++) {
+            count = 0;
+            char[] num = (Integer.toString(j)).toCharArray();
+            for (int i=0; i<num.length; i++) {
+                System.out.println(Character.getNumericValue(num[i]));
+                count += Character.getNumericValue(num[i]);
+            }
+            if (j+count == n) {
+                list.add(j);
+            }
+        }
+
+        if (list.isEmpty() == true) {
+            bw.write("0");
+        } else {
+            bw.write(list.get(0));
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
