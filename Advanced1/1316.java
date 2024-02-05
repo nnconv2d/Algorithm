5dfd6659f14af91389e98c88e28bbebbcb55d09b
Seoyeon Park <101249857+wwwept@users.noreply.github.com>
2024-02-06 00:12:04 +0900

    Update 1316.java

--- Advanced1/1316.java
+++ Advanced1/1316.java
@@ -1,26 +1,20 @@
 import java.io.*;
+import java.util.*;
 public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        int ans = -1;
+        int count = 0;
 
-        for (int i=1000; i>=0; i--) {
-            if (i*5 <= n) {
-                for (int j=0; j<1667; j++) {
-                    if (i*5 + j*3 == n) {
-                        ans = i+j;
-                    }
-                }
-            }
-            if (ans != -1) {
-                break;
-            }
+        for (int i=0; i<n; i++) {
+            List<Character> list = new ArrayList<>(Arrays.asList(br.readLine());
+
+
         }
 
-        bw.write(Integer.toString(ans));
+        bw.write(Integer.toString(count));
 
         br.close();
         bw.flush();
