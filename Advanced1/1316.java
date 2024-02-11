4bf9e8031af908dba9951fa6dad077807517799b
Seoyeon Park <101249857+wwwept@users.noreply.github.com>
2024-02-12 00:10:57 +0900

    Update 1316.java

--- Advanced1/1316.java
+++ Advanced1/1316.java
@@ -10,15 +10,11 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             List<Character> list = makeList(br);
-            for (int j = 0; j < list.size(); j++) {
-                if (Collections.frequency(list, list.get(j)) > 1) {
-                    boolean tf = isGroup(list, j);
-                    if (!tf) {
-                        break;
-                    } else {
-                        count++;
-                    }
-                }
+            boolean tf = isGroup(list, i);
+            if (!tf) {
+                break;
+            } else {
+                count++;
             }
             System.out.println(count);
         }
@@ -39,12 +35,19 @@ public class Main {
         return list;
     }
 
+    /** list의 index번째 알파벳이 list에 몇 번 들어있는지를 체크한 후
+     * 그 값이 2 이상일 경우 list에서의 알파벳 인덱스 리턴
+     * 이웃하는 인덱스의 차가 2 이상일 경우 false 리턴
+     */
     public static boolean isGroup(List list, int index) {
         int[] idx = new int[Collections.frequency(list, list.get(index))];
-        for (int i = 0; i < idx.length - 1; i++) {
-            String j = (list.get(index)).toString();
-            idx[i] = j.charAt(0);
-            if (idx[i + 1] - idx[i] > 1) {
+        for (int i=0; i<list.size(); i++) {
+            if (list.get(i).equals(list.get(index))) {
+                list.add(list.get(index));
+            }
+        }
+        for (int i=0; i<idx.length-1; i++) {
+            if (idx[i+1]- idx[i] > 1) {
                 return false;
             }
         }
