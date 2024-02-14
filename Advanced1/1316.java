2fb90b09fd0b7b35c0f294873449fe868b911fc2
Seoyeon Park <101249857+wwwept@users.noreply.github.com>
2024-02-14 21:17:23 +0900

    Update 1316.java

--- Advanced1/1316.java
+++ Advanced1/1316.java
@@ -35,22 +35,19 @@ public class Main {
         return list;
     }
 
-    /**
-     * list의 index번째 알파벳이 list에 몇 번 들어있는지를 체크한 후
-     * 그 값이 2 이상일 경우 list에서의 알파벳 인덱스 리턴
-     * 이웃하는 인덱스의 차가 2 이상일 경우 false 리턴
-     */
     public static boolean isGroup(List list, int index) {
-        if (Collections.frequency(list, list.get(index)) > 1) {
-            int[] idx = new int[Collections.frequency(list, list.get(index))];
-            for (int i = 0; i < list.size(); i++) {
-                if (list.get(i).equals(list.get(index))) {
-                    list.add(list.indexOf(list.get(index)));
+        for (int a=0; a<list.size(); a++) {
+            if (Collections.frequency(list, list.get(index)) > 1) {
+                int[] idx = new int[Collections.frequency(list, list.get(index))];
+                for (int i = 0; i < list.size(); i++) {
+                    if (list.get(i).equals(list.get(index))) {
+                        list.add(list.indexOf(list.get(index)));
+                    }
                 }
-            }
-            for (int i = 0; i < idx.length - 1; i++) {
-                if (idx[i + 1] - idx[i] > 1) {
-                    return false;
+                for (int i = 0; i < idx.length - 1; i++) {
+                    if (idx[i + 1] - idx[i] > 1) {
+                        return false;
+                    }
                 }
             }
         }
