b6908f7f6c6a67b952df8484c77c11db22417435
Seoyeon Park <101249857+wwwept@users.noreply.github.com>
2024-02-27 21:34:39 +0900

    Update 14425.java

--- Set&Map/14425.java
+++ Set&Map/14425.java
@@ -3,29 +3,28 @@ import java.util.*;
 public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        List<String> list1 = new ArrayList<>();
-        List<String> list2 = new ArrayList<>();
+        int count = 0;
+        List<String> list = new ArrayList<>();
 
         for (int i=0; i<n; i++) {
-            list1.add(br.readLine());
+            list.add(br.readLine());
         }
+
+        HashSet<String> set = new HashSet<>(list);
 
         for (int i=0; i<m; i++) {
-            list2.add(br.readLine());
+            String str = br.readLine();
+            if (set.contains(str)) {
+                count++;
+            }
         }
 
-        HashSet<String> set = new HashSet<>(list1);
-        HashSet<String> strs = new HashSet<>(list2);
+        System.out.print(count);
 
-        HashSet<String> intersection = new HashSet<>(set);
-        intersection.retainAll(strs);
-
-        System.out.print(intersection.size()+"");
+        br.close();
     }
 }
