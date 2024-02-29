ff90211a90dd0e5e9508ccb552a8ef603238e651
Seoyeon Park <101249857+wwwept@users.noreply.github.com>
2024-02-29 23:56:07 +0900

    Create 2750.java

---
+++ Sorting/2750.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+        for (int i=0; i<n; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(list);
+        for (int i=0; i<n; i++) {
+            bw.write(Integer.toString(list.get(i)));
+            bw.newLine();
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
