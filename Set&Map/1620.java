afc2298873aab675f50cce9a8ae7ad9ac89a7a3e
Seoyeon Park <101249857+wwwept@users.noreply.github.com>
2024-03-03 23:46:27 +0900

    Update 1620.java

--- Set&Map/1620.java
+++ Set&Map/1620.java
@@ -10,22 +10,21 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         Map<Integer, String> dict = new HashMap<>();
+        Map<String, Integer> dict2 = new HashMap<>();
 
         for (int i=1; i<=n; i++) {
-            dict.put(i, br.readLine());
+            String name = br.readLine();
+            dict.put(i, name);
+            dict2.put(name, i);
         }
 
         for (int i=0; i<m; i++) {
             String input = br.readLine();
-            int len = input.length();
-            if (len == 1) { // input이 숫자일 경우(자료형은 아직 String)
+            char inp = input.charAt(0);
+            if (inp < 58) { // input이 숫자일 경우(자료형은 아직 String)
                 bw.write(dict.get(Integer.parseInt(input)));
             } else { // input이 문자열일 경우
-                for (int key : dict.keySet()) {
-                    if (dict.get(key).equals(input)) {
-                        bw.write(Integer.toString(key));
-                    }
-                }
+                bw.write(Integer.toString(dict2.get(input)));
             }
             bw.newLine();
         }
