61fda28c89a7c7afcc8da049d35f74017fe9c1a0
Seoyeon Park <101249857+wwwept@users.noreply.github.com>
2024-03-23 23:56:28 +0900

    Update 2485.java

--- Numeric/2485.java
+++ Numeric/2485.java
@@ -12,7 +12,7 @@ public class Main {
         br.close();
 
         int[] distance = new int[n-1];
-        for (int i=0; i<n-1; i++) {
+        for (int i=0; i<n-1; i++) { 
             distance[i] = tree[i+1] - tree[i];
         }
         Arrays.sort(distance);
