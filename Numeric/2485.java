25f6a5cb7c163a16f1645470363ed57ea50439d1
Seoyeon Park <101249857+wwwept@users.noreply.github.com>
2024-03-27 23:29:39 +0900

    Update 2485.java

--- Numeric/2485.java
+++ Numeric/2485.java
@@ -9,38 +9,42 @@ public class Main {
         for (int i=0; i<n; i++) {
             tree[i] = Integer.parseInt(br.readLine());
         }
-        br.close(); 
-
-        int[] distance = new int[n-1];
-        for (int i=0; i<n-1; i++) { 
-            distance[i] = tree[i+1] - tree[i];
+        br.close();
+        int[] d = new int[n-1]; // distances between trees. length = n-1
+        for (int i=0; i<n-1; i++) {
+            d[i] = tree[i+1] - tree[i];
         }
-        Arrays.sort(distance);
+        Arrays.sort(d);
 
-        int min = distance[0];
-        int i = 0;
-
-        while (i < n-1) {
-            if (distance[i]%min != 0) {
-                min--;
-                i=0;
-                continue;
-            }
-            if (distance[i]%min == 0 && i==n-2) {
-                break;
-            }
-            i++; 
+        int gcd = getGcd(d[n-2], d[n-3]);
+        for (int i=n-3; i>=0; i--) {
+            gcd = getGcd(d[i], gcd);
         }
 
         int count = 0;
-
-        for (int j=tree[0]; j+min<=tree[n-1]; j+=min) {
-            if (!Arrays.asList(tree).contains(j)) {
+        List<Integer> treeArray = new ArrayList<>(Arrays.asList(tree));
+        for (int i=tree[0]; i<=tree[n-1]; i+=gcd) {
+            if (!treeArray.contains(i)) {
                 count++;
             }
         }
 
         System.out.println(count);
 
+    }
+
+    private static int getGcd(int a, int b) {
+        int max = Math.max(a, b);
+        int min = Math.min(a, b);
+
+        while (true) {
+            int remain = max%min;
+            if (remain == 0) {
+                return min;
+            } else {
+                max = min;
+                min = remain;
+            }
+        }
     }
 }
