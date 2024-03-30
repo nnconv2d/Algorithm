3eef2eb0d9959d67a65e35fc25205e7cfa42e7c1
Seoyeon Park <101249857+wwwept@users.noreply.github.com>
2024-03-30 20:37:03 +0900

    Update 2485.java

--- Numeric/2485.java
+++ Numeric/2485.java
@@ -1,50 +1,38 @@
+// 기본 알고리즘은 다 너무너무 잘 생각했는데 필요없는 배열, 변수들 때문에 계속 시간 초과 나던 문제
+// 없어도 되는 배열은 만들지 말기! 웬만하면 일회용 변수 선에서 처리
+
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        Integer[] tree = new Integer[n];
+        int[] tree = new int[n];
         for (int i=0; i<n; i++) {
             tree[i] = Integer.parseInt(br.readLine());
         }
         br.close();
-        int[] d = new int[n-1]; // distances between trees. length = n-1
+
+        int gcd = tree[n-1] - tree[n-2];
         for (int i=0; i<n-1; i++) {
-            d[i] = tree[i+1] - tree[i];
-        }
-        Arrays.sort(d);
-
-        int gcd = getGcd(d[n-2], d[n-3]);
-        for (int i=n-3; i>=0; i--) {
-            gcd = getGcd(d[i], gcd);
+            int d = tree[i+1] - tree[i];
+            gcd = getGcd(d, gcd);
         }
 
-        int count = 0;
-        List<Integer> treeArray = new ArrayList<>(Arrays.asList(tree));
-        for (int i=tree[0]; i<=tree[n-1]; i+=gcd) {
-            if (!treeArray.contains(i)) {
-                count++;
-            }
-        }
+        bw.write(Integer.toString((tree[tree.length-1] - tree[0])/gcd +1 - tree.length));
 
-        System.out.println(count);
+        bw.flush();
+        bw.close();
 
     }
 
     private static int getGcd(int a, int b) {
-        int max = Math.max(a, b);
-        int min = Math.min(a, b);
-
-        while (true) {
-            int remain = max%min;
-            if (remain == 0) {
-                return min;
-            } else {
-                max = min;
-                min = remain;
-            }
+        while (b != 0) {
+            int r = a%b;
+            a = b;
+            b = r;
         }
+        return a;
     }
 }
